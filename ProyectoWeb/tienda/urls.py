0413a71c6eaaa187d9eca25d249265313023825a
yaplin1 <160192966+yaplin1@users.noreply.github.com>
2025-09-12 17:57:34 -0500

    Update urls.py

--- ProyectoWeb/tienda/urls.py
+++ ProyectoWeb/tienda/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     
     path('',views.tienda, name="Tienda"),
     path('categoria/<int:categorias_id>', views.productos_por_categoria, name="productos_por_categoria"),
+    path('buscar/', views.buscar_productos, name="buscar_productos"), 
     
     # path('productos/', views.lista_todos_los_productos, name="todos_los_productos"),
 
