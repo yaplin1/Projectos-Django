eb338676352167e02ac2cb8e647956a2585ac8ab
yaplin1 <160192966+yaplin1@users.noreply.github.com>
2025-09-12 18:01:39 -0500

    Update views.py

--- ProyectoWeb/tienda/views.py
+++ ProyectoWeb/tienda/views.py
@@ -16,3 +16,16 @@ def productos_por_categoria(request, categorias_id):
     productos=Producto.objects.filter(categorias=categoria, disponibilidad=True)
     return render(request, "tienda/productos_por_categoria.html", {'categoria_actual':categoria,'productos': productos, 'categorias_disponibles': CategoriaProd.objects.all() })
 
+# Nueva vista para el buscador
+def buscar_productos(request):
+    query = request.GET.get('q')
+    #print("Valor recibido:", query)  # <-- Esto te ayuda a depurar
+    productos = Producto.objects.all() # Traer todos los productos por defecto
+
+    if query:
+        # Usamos Q objects para buscar en nombre o en la descripción del producto
+        productos = productos.filter(
+            Q(nombre__icontains=query) 
+        ).distinct()
+
+    return render(request, 'tienda/resultados_busqueda.html', {'productos': productos, 'query': query})
